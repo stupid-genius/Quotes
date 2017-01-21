d4b123d8265741738217e7527f7d68d9d3f662f4
Allen Ng <ang@appnexus.com>
2017-01-21 16:37:24 -0700

    first draft basic functionality working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,64 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+'use strict';
+
+/* TODO
+change text size of quote to fit to div
+add random mode
+? allow filtering
+allow selection from url
+ ? change UI to be single
+add MD service to allow MD in quotes
+*/
+var quotesApp = angular.module('quotesApp', ['ngRoute']);
+
+quotesApp.config(['$routeProvider', function($routeProvider){
+}]);
+
+quotesApp.factory('quotesFactory', ['$q', '$http', function($q, $http){
+	var getQuotes = $q.defer();
+	$http.get('scripts/quotes.json')
+	.success(function(list){
+		var quotes = list.sort().map(function(e){
+			var pair = e.split(/::/);
+			return {
+				name: pair[0],
+				quote: pair[1]
+			};
+		});
+		getQuotes.resolve(quotes);
+	})
+	.error(function(e){
+		console.error(e);
+	});
+
+	return getQuotes.promise.then(function(quotes){
+		var quotesFactory = {};
+		quotesFactory.get = function(index){
+			return quotes[index];
+		};
+		quotesFactory.start = function(){
+
+		};
+		quotesFactory.next = function(){
+
+		};
+		quotesFactory.prev = function(){
+
+		};
+		quotesFactory.all = function(){
+			return quotes;
+		};
+		return quotesFactory;
+	});
+}]);
+
+quotesApp.controller('quotesController', ['$scope', 'quotesFactory', function($scope, getQuotes){
+	getQuotes.then(function(quotesFactory){
+		console.log(quotesFactory);
+		$scope.quotes = quotesFactory.all();
+		$scope.selectQuote = function(id){
+			var quote = quotesFactory.get(id);
+			$scope.quote = quote.quote;
+		};
+	});
+}]);
