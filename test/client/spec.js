12eb23be25df6b6e8f2975382486937b3d6a23bd
Allen Ng <accounts@stupid-genius.com>
2023-12-07 00:28:37 -0500

    Refactor to new scaffolding
    Including publish via GHA

---
+++ test/client/spec.js
@@ -0,0 +1,9 @@
+describe('Client Tests', function() {
+	console.log('Please write some tests; I\'m lonely');
+	it('should have tests', function() {
+		expect(true).to.be.true;
+	});
+	it('should have more than 1 test', function() {
+		expect(1 + 1).to.equal(2);
+	});
+});
